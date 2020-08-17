e394cac58663bf9ff987b653bdf96cf8ef3b743f
Kauã Machado <gnomoscorpions@gmail.com>
2020-08-17 08:44:50 -0300

    categorias do site feito

---
+++ views-cache/categories-menu.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Adubos</a></li><li><a href="/categories/6">Agrotóxicos</a></li><li><a href="/categories/2">Cereais</a></li><li><a href="/categories/3">Hortaliças</a></li>
