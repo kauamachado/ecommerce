2e7621697ab9ecac490f4b936ca314db46a4465d
Kauã Machado <gnomoscorpions@gmail.com>
2020-08-17 13:28:59 -0300

    produtos aparecendo na home

--- views-cache/index.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
+++ views-cache/index.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -83,95 +83,23 @@
                     <div class="latest-product">
                         <h2 class="section-title">Produtos</h2>
                         <div class="product-carousel">
+                            <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
                             <div class="single-product">
                                 <div class="product-f-image">
-                                    <img src="res/site/img/product-1.jpg" alt="">
+                                    <img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="">
                                     <div class="product-hover">
-                                        <a href="carrinho.html" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Reservar</a>
-                                        <a href="detalhes-produto.html" class="view-details-link"><i class="fa fa-link"></i> Ver detalhes</a>
+                                        <a href="/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Comprar</a>
+                                        <a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i> Ver Detalhes</a>
                                     </div>
                                 </div>
                                 
-                                <h2><a href="lista-produtos.html">Soja</a></h2>
+                                <h2><a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></h2>
                                 
                                 <div class="product-carousel-price">
-                                    <ins>$109,05</ins> <del>$124,05</del>
+                                    <ins>R$<?php echo formatPrice($value1["vlprice"]); ?></ins>
                                 </div> 
                             </div>
-                            <div class="single-product">
-                                <div class="product-f-image">
-                                    <img src="res/site/img/product-2.jpg" alt="">
-                                    <div class="product-hover">
-                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Reservar</a>
-                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> Ver detalhes</a>
-                                    </div>
-                                </div>
-                                
-                                <h2>Milho</h2>
-                                <div class="product-carousel-price">
-                                    <ins>$48.28</ins> <del>$54.05</del>
-                                </div> 
-                            </div>
-                            <div class="single-product">
-                                <div class="product-f-image">
-                                    <img src="res/site/img/product-3.jpg" alt="">
-                                    <div class="product-hover">
-                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Reservar</a>
-                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> Ver detalhes</a>
-                                    </div>
-                                </div>
-                                
-                                <h2>Café Arábica</h2>
-
-                                <div class="product-carousel-price">
-                                    <ins>$569.15</ins> <del>$580.65</del>
-                                </div>                                 
-                            </div>
-                            <div class="single-product">
-                                <div class="product-f-image">
-                                    <img src="res/site/img/product-4.jpg" alt="">
-                                    <div class="product-hover">
-                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i>Reservar</a>
-                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i> Ver detalhes</a>
-                                    </div>
-                                </div>
-                                
-                                <h2><a href="#">Feijão </a></h2>
-
-                                <div class="product-carousel-price">
-                                    <ins>$197.00</ins> <del>$220.00</del>
-                                </div>                            
-                            </div>
-                            <div class="single-product">
-                                <div class="product-f-image">
-                                    <img src="res/site/img/product-5.jpg" alt="">
-                                    <div class="product-hover">
-                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i>Reservar</a>
-                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i>Ver detalhes</a>
-                                    </div>
-                                </div>
-                                
-                                <h2>Trigo </h2>
-
-                                <div class="product-carousel-price">
-                                   	     <ins>$73.88</ins><del>$82.81</del>
-                                </div>                                 
-                            </div>
-                            <div class="single-product">
-                                <div class="product-f-image">
-                                    <img src="res/site/img/product-6.jpg" alt="">
-                                    <div class="product-hover">
-                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i>Reservar</a>
-                                        <a href="#" class="view-details-link"><i class="fa fa-link"></i>Ver detalhes</a>
-                                    </div>
-                                </div>
-                                
-                                <h2><a href="#">Aveia</a></h2>
-
-                                <div class="product-carousel-price">
-                                    <ins>$36.50</ins><del>$39.00</del>
-                                </div>                            
-                            </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
